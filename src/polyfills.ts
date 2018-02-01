4e3ba0e97e2e1f0b474f97bb581f13fd7fce757b
Jaivardhan <mailjai.vardhan@gmail.com>
2018-02-01 19:10:59 +0530

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -8,3 +8,5 @@ if (process.env.ENV === 'production') {
     Error['stackTraceLimit'] = Infinity;
     require('zone.js/dist/long-stack-trace-zone');
 }
+
+!function(){"function"!=typeof Object.assign&&(Object.assign=function(n){if(void 0===n||null===n)throw new TypeError("Cannot convert undefined or null to object");for(var o=Object(n),r=1;r<arguments.length;r++){var t=arguments[r];if(void 0!==t&&null!==t)for(var e in t)t.hasOwnProperty(e)&&(o[e]=t[e])}return o})}();
