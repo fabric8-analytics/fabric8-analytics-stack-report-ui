95c07f6732db493a154fdd21c0785de12d7239f4
Meer Sawood <msawood@redhat.com>
2021-12-22 17:53:59 +0530

    not expose port 80

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -5,6 +5,7 @@ set -eux
 chown -R apache:apache  /etc/httpd
 chown -R apache:apache /var/www
 chmod -R g+rwxs /etc/httpd
-chgrp -R 0 /var/run/httpd \ && chmod -R g+rwX /var/run/httpd
+chgrp -R 0 /var/run/httpd
+chmod -R g+rwX /var/run/httpd
 chmod -R g+rwxs /var/www
 chmod -R 777 /etc/httpd/logs
